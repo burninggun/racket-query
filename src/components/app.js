efe4ca109e4abff25c8922a64b56e3de6e70b826
William Cheng <willchengsters@gmail.com>
2019-08-26 11:45:04 -0700

    material-ui testing

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import '../assets/css/app.scss';
-import logo from '../assets/images/logo.svg';
+import AppBar from './AppBar'
+import Racket_Input from './racket_Input'
+import '../assets/css/app.scss'
+
+import Container from '@material-ui/core/Container';
 
 const App = () => (
     <div>
-        <div className="app">
-            <img src={logo} className="logo rotate"/>
-            <h1>Welcome To React</h1>
-        </div>
+        <AppBar maxWidth='sm' />
+        <Racket_Input/>
     </div>
 );
 
